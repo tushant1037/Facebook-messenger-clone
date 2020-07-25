e80d7c540d553ab73eef2e96231dd1943f741526
tushant1037 <42100482+tushant1037@users.noreply.github.com>
2020-07-25 12:08:54 +0530

    corona tracker

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{Component} from 'react';
 import './App.css';
+import Header from './Components/header';
+import Body from './Components/body';
+import {totalData} from './Components/Api/index';
 
-function App() {
+class App extends Component {
+  state = 
+    {
+    totalConfirmed : '',
+    totalDeath : '',
+    totalRecovered : ''
+  }
+
+  componentDidMount = async() => {
+    await totalData().then(res => {
+      this.setState({
+        totalConfirmed : res.data.TotalConfirmed,
+        totalDeath : res.data.TotalDeaths,
+        totalRecovered : res.data.TotalRecovered   
+      })
+    })
+    .catch(err => {
+      console.log("error = ", err);
+    })
+  }
+
+
+  render() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+      <Header totalConfirmed={this.state.totalConfirmed} 
+              totalDeath={this.state.totalDeath} 
+              totalRecovered={this.state.totalRecovered}/>
+      </div>
+      <div>
+        <Body />
+      </div>
     </div>
   );
+  }
 }
 
 export default App;
